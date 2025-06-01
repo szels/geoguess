7dff6bc08314c625344fc6191a9c8cd06213d964
Steffen <steffen@uberall.com>
2025-06-01 20:20:03 +0200

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders quiz title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Asiatische Hauptstädte Quiz/i);
-  expect(titleElement).toBeInTheDocument();
-});
